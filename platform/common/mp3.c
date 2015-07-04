68cf40ec367825dc5a2f7a94c3d65595ecde7a33
twinaphex <libretro@gmail.com>
2015-07-04 19:19:32 +0200

    Change includes so that things build without include dir shenanigans

--- platform/common/mp3.c
+++ platform/common/mp3.c
@@ -8,8 +8,8 @@
 #include <stdio.h>
 #include <string.h>
 
-#include <pico/pico_int.h>
-#include <pico/sound/mix.h>
+#include "../../pico/pico_int.h"
+#include "../../pico/sound/mix.h"
 #include "mp3.h"
 
 static FILE *mp3_current_file;
