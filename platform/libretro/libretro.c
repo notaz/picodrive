2d4f989df99867149b8fe9a7f2d9a11799f469af
Gregor Richards <hg-yff@gregor.im>
2016-12-09 18:50:07 -0500

    Report git version with library_version

--- platform/libretro/libretro.c
+++ platform/libretro/libretro.c
@@ -539,7 +539,10 @@ void retro_get_system_info(struct retro_system_info *info)
 {
    memset(info, 0, sizeof(*info));
    info->library_name = "PicoDrive";
-   info->library_version = VERSION;
+#ifndef GIT_VERSION
+#define GIT_VERSION ""
+#endif
+   info->library_version = VERSION GIT_VERSION;
    info->valid_extensions = "bin|gen|smd|md|32x|cue|iso|sms";
    info->need_fullpath = true;
 }
