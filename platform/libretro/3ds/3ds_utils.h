fd587b673e51820d82cfeec142048a97faeafd05
notaz <notasas@gmail.com>
2018-01-31 23:44:45 +0200

    ctr/3ds: attempt to improve cache flushing

--- platform/libretro/3ds/3ds_utils.h
+++ platform/libretro/3ds/3ds_utils.h
@@ -1,9 +1,6 @@
 #ifndef _3DS_UTILS_H
 #define _3DS_UTILS_H
 
-void ctr_invalidate_ICache(void);
-void ctr_flush_DCache(void);
-
 void ctr_flush_invalidate_cache(void);
 
 int ctr_svchack_init(void);
