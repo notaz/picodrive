b9fd66b28d7ccda4008cb314b0c5ffad1554900c
Francisco Javier Trujillo Mata <fjtrujy@gmail.com>
2020-06-07 14:22:14 +0200

    Change GSKit PS2 version

--- platform/libretro/libretro-common/include/libretro_gskit_ps2.h
+++ platform/libretro/libretro-common/include/libretro_gskit_ps2.h
@@ -1,4 +1,4 @@
-/* Copyright (C) 2010-2018 The RetroArch team
+/* Copyright (C) 2010-2020 The RetroArch team
  *
  * ---------------------------------------------------------------------------------------------
  * The following license statement only applies to this libretro API header (libretro_d3d.h)
@@ -33,7 +33,7 @@
 
 #include <gsKit.h>
 
-#define RETRO_HW_RENDER_INTERFACE_GSKIT_PS2_VERSION 1
+#define RETRO_HW_RENDER_INTERFACE_GSKIT_PS2_VERSION 2
 
 struct retro_hw_ps2_insets
 {
