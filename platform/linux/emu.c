a09f88adce40d9f90086de79df232cc4727b2ca0
kub <derkub@gmail.com>
2020-06-23 23:36:38 +0200

    SDL UI, fix for CD LED display

--- platform/linux/emu.c
+++ platform/linux/emu.c
@@ -39,10 +39,11 @@ static void draw_cd_leds(void)
 	int led_reg, pitch, scr_offs, led_offs;
 	led_reg = Pico_mcd->s68k_regs[0];
 
-	pitch = 320;
+	pitch = g_screen_ppitch;
 	led_offs = 4;
 	scr_offs = pitch * 2 + 4;
 
+#if 0
 	if (currentConfig.renderer != RT_16BIT) {
 	#define p(x) px[(x) >> 2]
 		// 8-bit modes
@@ -52,7 +53,9 @@ static void draw_cd_leds(void)
 		p(pitch*0) = p(pitch*1) = p(pitch*2) = col_g;
 		p(pitch*0 + led_offs) = p(pitch*1 + led_offs) = p(pitch*2 + led_offs) = col_r;
 	#undef p
-	} else {
+	} else
+#endif
+	{
 	#define p(x) px[(x)*2 >> 2] = px[((x)*2 >> 2) + 1]
 		// 16-bit modes
 		unsigned int *px = (unsigned int *)((short *)g_screen_ptr + scr_offs);
