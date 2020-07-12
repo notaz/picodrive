e6a52e1940bb0402b85d1b72a0835c7ea83ff1e0
twinaphex <libretro@gmail.com>
2020-07-12 19:10:14 +0200

    Prevent collission with PS2 SDK

--- pico/pico_types.h
+++ pico/pico_types.h
@@ -3,6 +3,7 @@
 
 #include <stdint.h>
 
+#ifndef __TAMTYPES_H__
 #ifndef UTYPES_DEFINED
 typedef uint8_t        u8;
 typedef int8_t         s8;
@@ -11,6 +12,8 @@ typedef int16_t        s16;
 typedef uint32_t       u32;
 typedef int32_t        s32;
 #endif
+#endif
+
 typedef uintptr_t      uptr; /* unsigned pointer-sized int */
 
 #endif
