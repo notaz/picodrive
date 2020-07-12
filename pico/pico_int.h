663b3566080ed7fb752faa5f8766835df82fe60a
twinaphex <libretro@gmail.com>
2020-07-13 01:33:41 +0200

    Fix more conflicting types for prototypes

--- pico/pico_int.h
+++ pico/pico_int.h
@@ -955,8 +955,8 @@ void p32x_schedule_hint(SH2 *sh2, unsigned int m68k_cycles);
 
 // 32x/memory.c
 extern struct Pico32xMem *Pico32xMem;
-unsigned int PicoRead8_32x(u32 a);
-unsigned int PicoRead16_32x(u32 a);
+u32 PicoRead8_32x(u32 a);
+u32 PicoRead16_32x(u32 a);
 void PicoWrite8_32x(u32 a, u32 d);
 void PicoWrite16_32x(u32 a, u32 d);
 void PicoMemSetup32x(void);
@@ -964,10 +964,10 @@ void Pico32xSwapDRAM(int b);
 void Pico32xMemStateLoaded(void);
 void p32x_update_banks(void);
 void p32x_m68k_poll_event(u32 flags);
-unsigned int REGPARM(3) p32x_sh2_poll_memory8(u32 a, u32 d, SH2 *sh2);
-unsigned int REGPARM(3) p32x_sh2_poll_memory16(u32 a, u32 d, SH2 *sh2);
-unsigned int REGPARM(3) p32x_sh2_poll_memory32(u32 a, u32 d, SH2 *sh2);
-void *p32x_sh2_get_mem_ptr(unsigned int a, unsigned int *mask, SH2 *sh2);
+u32 REGPARM(3) p32x_sh2_poll_memory8(u32 a, u32 d, SH2 *sh2);
+u32 REGPARM(3) p32x_sh2_poll_memory16(u32 a, u32 d, SH2 *sh2);
+u32 REGPARM(3) p32x_sh2_poll_memory32(u32 a, u32 d, SH2 *sh2);
+void *p32x_sh2_get_mem_ptr(u32 a, u32 *mask, SH2 *sh2);
 void p32x_sh2_poll_detect(u32 a, SH2 *sh2, u32 flags, int maxcnt);
 void p32x_sh2_poll_event(SH2 *sh2, u32 flags, u32 m68k_cycles);
 int p32x_sh2_memcpy(u32 dst, u32 src, int count, int size, SH2 *sh2);
