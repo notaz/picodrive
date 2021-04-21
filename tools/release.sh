64cb48792833d7e1c6321af16c6fb0fde216e413
kub <derkub@gmail.com>
2021-04-21 22:30:58 +0200

    build, tiny release script improvement

--- tools/release.sh
+++ tools/release.sh
@@ -15,7 +15,7 @@
 #	rg350:		rg350-toolchain (+ mips-toolchain for newer gcc)
 #	psp:		pspdev
 
-mkdir -p release
+mkdir -p release-$1
 trap "exit" ERR
 
 # GPH devices: gp2x, wiz, caanoo, with ubuntu arm gcc 4.7
@@ -23,18 +23,18 @@ trap "exit" ERR
 TC=$HOME/opt/open2x/gcc-4.1.1-glibc-2.3.6 CROSS_COMPILE=arm-linux-gnueabi- CFLAGS="-I$TC/arm-open2x-linux/include -I$HOME/src/gp2x/armroot/include -U_FORTIFY_SOURCE" LDFLAGS="-B$TC/lib/gcc/arm-open2x-linux/4.1.1 -B$TC/arm-open2x-linux/lib -L$TC/arm-open2x-linux/lib -L$HOME/src/gp2x/armroot/lib" ./configure --platform=gp2x
 make clean all
 make -C platform/gp2x rel VER=$1
-mv PicoDrive_$1.zip release/PicoDrive-gph_$1.zip
+mv PicoDrive_$1.zip release-$1/PicoDrive-gph_$1.zip
 
 # dingux: dingoo a320, ritmix rzx-50, JZ4755 or older (mips32r1 w/o fpu)
 # NB works for legacy dingux and possibly opendingux before gcw0
 TC=$HOME/opt/opendingux-toolchain PATH=$TC/usr/bin:$PATH CROSS_COMPILE=mipsel-linux- CFLAGS="-I $TC/usr/include -I $TC/usr/include/SDL -Wno-unused-result -mabicalls" LDFLAGS="--sysroot $TC" ./configure --platform=dingux
 TC=$HOME/opt/opendingux-toolchain PATH=$TC/usr/bin:$PATH make clean all
-mv PicoDrive-dge.zip release/PicoDrive-dge_$1.zip
+mv PicoDrive-dge.zip release-$1/PicoDrive-dge_$1.zip
 
 # retrofw: rs-97 and similar, JZ4760 (mips32r1 with fpu)
 TC=$HOME/opt/mipsel-linux-uclibc PATH=$TC/bin:$PATH CROSS_COMPILE=mipsel-linux- CFLAGS="-I $TC/include -I $TC/include/SDL -Wno-unused-result" LDFLAGS="--sysroot $TC/mipsel-buildroot-linux-uclibc/sysroot" ./configure --platform=retrofw
 TC=$HOME/opt/mipsel-linux-uclibc PATH=$TC/bin:$PATH make clean all
-mv PicoDrive.opk release/PicoDrive-retrofw_$1.opk
+mv PicoDrive.opk release-$1/PicoDrive-retrofw_$1.opk
 
 # gcw0: JZ4770 (mips32r2 with fpu), swapped X/Y buttons, SDK toolchain
 #TC=$HOME/opt/gcw0-toolchain PATH=$TC/usr/bin:$PATH CROSS_COMPILE=mipsel-linux- TS=$TC/usr/mipsel-gcw0-linux-uclibc/sysroot CFLAGS="-I$TS/usr/include -I$TS/usr/include/SDL" LDFLAGS="--sysroot $TS" ./configure --platform=gcw0
@@ -43,7 +43,7 @@ mv PicoDrive.opk release/PicoDrive-retrofw_$1.opk
 # gcw0: JZ4770 (mips32r2 with fpu), swapped X/Y buttons, newer generic gcc
 TC=$HOME/opt/gcw0-toolchain PATH=$HOME/opt/mips-toolchain/bin:$PATH CROSS_COMPILE=mipsel-linux- TS=$TC/usr/mipsel-gcw0-linux-uclibc/sysroot CFLAGS="-I$TS/usr/include -I$TS/usr/include/SDL -mabicalls" LDFLAGS="--sysroot $TS -Wl,--dynamic-linker=/lib/ld-uClibc.so.0" ./configure --platform=gcw0
 PATH=$HOME/opt/mips-toolchain/usr/bin:$PATH make clean all
-mv PicoDrive.opk release/PicoDrive-gcw0_$1.opk
+mv PicoDrive.opk release-$1/PicoDrive-gcw0_$1.opk
 
 # rg350: JZ4770, SDK toolchain
 #TC=$HOME/opt/rg350-toolchain PATH=$TC/usr/bin:$PATH CROSS_COMPILE=mipsel-linux- TS=$TC/usr/mipsel-gcw0-linux-uclibc/sysroot CFLAGS="-I$TS/usr/include -I$TS/usr/include/SDL" LDFLAGS="--sysroot $TS" ./configure --platform=rg350
@@ -53,10 +53,10 @@ mv PicoDrive.opk release/PicoDrive-gcw0_$1.opk
 # NB this may or may not work on the newer opendingux beta
 TC=$HOME/opt/rg350-toolchain PATH=$HOME/opt/mips-toolchain/bin:$PATH CROSS_COMPILE=mipsel-linux- TS=$TC/usr/mipsel-gcw0-linux-uclibc/sysroot CFLAGS="-I$TS/usr/include -I$TS/usr/include/SDL -mabicalls" LDFLAGS="--sysroot $TS -Wl,--dynamic-linker=/lib/ld-uClibc.so.0" ./configure --platform=opendingux
 PATH=$HOME/opt/mips-toolchain/usr/bin:$PATH make clean all
-mv PicoDrive.opk release/PicoDrive-opendingux_$1.opk
+mv PicoDrive.opk release-$1/PicoDrive-opendingux_$1.opk
 
 # psp (experimental), pspdev SDK toolchain
 TC=$HOME/opt/pspdev PATH=$TC/bin:$PATH CROSS_COMPILE=psp- CFLAGS="-I$TC/psp/sdk/include -D_POSIX_C_SOURCE=199506L" LDFLAGS="-L$TC/psp/sdk/lib" ./configure --platform=psp
 TC=$HOME/opt/pspdev PATH=$TC/bin:$PATH make clean all
 make -C platform/psp rel VER=$1
-mv PicoDrive_psp_$1.zip release/PicoDrive-psp_$1.zip
+mv PicoDrive_psp_$1.zip release-$1/PicoDrive-psp_$1.zip
