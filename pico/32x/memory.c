946c3b0ef56251d45628b2e4b19b6b0b54df995d
kub <derkub@gmail.com>
2022-10-08 11:03:42 +0000

    32x, make 68k poll detection less agressive

--- pico/32x/memory.c
+++ pico/32x/memory.c
@@ -59,7 +59,7 @@ static void (*m68k_write16_io)(u32 a, u32 d);
 #define REG8IN16(ptr, offs) ((u8 *)ptr)[MEM_BE2(offs)]
 
 // poll detection
-#define POLL_THRESHOLD 5
+#define POLL_THRESHOLD 9  // cosmic carnage
 
 static struct {
   u32 addr1, addr2, cycles;
