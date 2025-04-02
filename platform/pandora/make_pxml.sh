26ecb2b6358fefba24e3d68b9eb2efba7f10d5ee
notaz <notasas@gmail.com>
2025-04-03 02:33:00 +0300

    pandora: fix readme and pxml version

--- platform/pandora/make_pxml.sh
+++ platform/pandora/make_pxml.sh
@@ -7,7 +7,7 @@ test -f $verfile
 major=`head -n 1 $verfile | sed 's/[^0-9]*\([0-9]*\)\.\([0-9]*\).*/\1/g'`
 minor=`head -n 1 $verfile | sed 's/[^0-9]*\([0-9]*\)\.\([0-9]*\).*/\2/g'`
 # lame, I know..
-build=`git describe HEAD | grep -- - | sed -e 's/.*\-\(.*\)\-.*/\1/'`
+build=`git describe --tags HEAD | grep -- - | sed -e 's/.*\-\(.*\)\-.*/\1/'`
 test -n "$build" && build_post="-$build"
 test -n "$build" || build=0
 
